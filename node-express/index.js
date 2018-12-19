14f4c379b9d0481cb210bfe644e8c0106a50c7ea
Tomas Izo <tomas@izo.sk>
2018-12-19 19:38:53 +0900

    refactoring

--- node-express/index.js
+++ node-express/index.js
@@ -1,20 +1,25 @@
+// dependencies
 const express = require('express'), 
     http = require('http'), 
-    morgan = require('morgan'),
-    hostname = 'localhost', 
+    morgan = require('morgan');
+
+// constants
+const hostname = 'localhost', 
     port = 3000;
 
+
 const app = express();
+
 app.use(morgan('dev'));
-
 app.use(express.static(__dirname + '/public'));
-
 app.use((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>')
 });
 
+
+// run server
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {

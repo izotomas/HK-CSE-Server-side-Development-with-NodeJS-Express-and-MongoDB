10a4b9e929cc5b58068858e6f477af6983fb04dc
Tomas Izo <tomasizo@Tomass-MacBook-Pro.local>
2019-01-16 21:22:11 +0100

    assignment 2 task 1

---
+++ assignments/02/models/promotions.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+const promoSchema = new Schema({
+    name:  {
+        type: String,
+        required: true
+    },
+    image:  {
+        type: String,
+        required: true
+    },
+    label:  {
+        type: String,
+        required: false,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: false, //bug: true throws exception??
+    },
+    featured: {
+        type: Boolean
+    }
+},
+{
+    timestamps: true
+});
+
+const Promotions = mongoose.model('Promotion', promoSchema);
+
+module.exports = Promotions;

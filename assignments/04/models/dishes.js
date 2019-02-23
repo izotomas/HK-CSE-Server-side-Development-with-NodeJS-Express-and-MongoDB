fdcbd69874d53f84769a91e9c2510553c718a08f
Tomas Izo <tomas@izo.sk>
2019-02-23 14:20:29 +0900

    linting

--- assignments/04/models/dishes.js
+++ assignments/04/models/dishes.js
@@ -6,61 +6,61 @@ require('mongoose-currency').loadType(mongoose);
 var Currency = mongoose.Types.Currency;
 
 const commentSchema = new Schema({
-    rating:  {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5,
+            required: true
+        },
+        comment: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
     },
-    comment:  {
-        type: String,
-        required: true
-    },
-    author:  {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }
-},
-{
-    timestamps: true
-});
+    {
+        timestamps: true
+    }),
 
-const dishSchema = new Schema({
-    name: {
-	    type: String,
-	    required: true,
-	    unique: true
-    },
-    description: {
-	    type: String,
-	    required: true
-    },
-    image: {
-	    type: String,
-	    required: true
-    },
-    category: {
-	    type: String,
-	    required: true
-    },
-    label: {
-	    type: String,
-	    default: ''
-    },
-    price: {
-	    type: Currency,
-	    required: false,
-	    min: 0
-    },
-    featured: {
-	    type: Boolean,
-	    default: false
-    },
-    comments: [commentSchema]
-}, {
-    timestamps: true
-});
+    dishSchema = new Schema({
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        category: {
+            type: String,
+            required: true
+        },
+        label: {
+            type: String,
+            default: ''
+        },
+        price: {
+            type: Currency,
+            required: false,
+            min: 0
+        },
+        featured: {
+            type: Boolean,
+            default: false
+        },
+        comments: [commentSchema]
+    }, {
+        timestamps: true
+    }),
 
-const Dishes = mongoose.model('Dish', dishSchema);
+    Dishes = mongoose.model('Dish', dishSchema);
 
 module.exports = Dishes;
--- assignments/04/models/leaders.js
+++ assignments/04/models/leaders.js
@@ -3,35 +3,35 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const leaderSchema = new Schema({
-    name:  {
-        type: String,
-        required: true
+        name: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        designation: {
+            type: String,
+            required: true
+        },
+        abbr: {
+            type: String,
+            required: false,
+            default: ''
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        featured: {
+            type: Boolean
+        }
     },
-    image:  {
-        type: String,
-        required: true
-    },
-    designation:  {
-        type: String,
-        required: true
-    },
-    abbr: {
-        type: String,
-        required: false,
-        default: ''
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    featured: {
-        type: Boolean
-    }
-},
-{
-    timestamps: true
-});
+    {
+        timestamps: true
+    }),
 
-const Leaders = mongoose.model('Leader', leaderSchema);
+    Leaders = mongoose.model('Leader', leaderSchema);
 
 module.exports = Leaders;
